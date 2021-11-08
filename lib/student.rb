0a225863f5efa5afbb7c6eb00d05b9bdc6dc6b48
Aristela Marku <aristela.marku@gmail.com>
2021-11-08 14:40:34 +0100

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,16 @@
-class Student 
+require "user.rb"
+
+class Student < User
+    attr_reader :knowledge
+    def initialize
+      @knowledge = []
+    end
+    def learn(knowledge)
+      @knowledge << knowledge
+    end
+
+    def knowledge
+     @knowledge
+    end
 
 end
